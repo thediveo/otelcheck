530bc0576567f7818e2348e48730f73c76f13cbf
thediveo <thediveo@gmx.eu>
2025-09-29 17:30:58 +0000

    wip: complete Index unit testing
    Signed-off-by: thediveo <thediveo@gmx.eu>

--- motel/metrics/index_test.go
+++ motel/metrics/index_test.go
@@ -17,12 +17,132 @@ package metrics
 import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/sdk/metric/metricdata"
 )
 
 var _ = Describe("metrics indices", func() {
 
-	It("rejects the same-name metrics with differing aggregations", func() {
+	Context("same-name metrics with differing properties", func() {
+
+		It("rejects differing units", func() {
+			rms := []*metricdata.ResourceMetrics{
+				{
+					ScopeMetrics: []metricdata.ScopeMetrics{
+						{
+							Metrics: []metricdata.Metrics{
+								{
+									Name: "foo.bar",
+									Unit: "kilofubars",
+									Data: metricdata.Gauge[int64]{
+										DataPoints: []metricdata.DataPoint[int64]{
+											{
+												Value:      42,
+												Attributes: attribute.NewSet(attribute.String("foo", "bar")),
+											},
+										},
+									},
+								},
+								{
+									Name: "foo.bar",
+									Unit: "megafubars",
+									Data: metricdata.Sum[int64]{
+										DataPoints: []metricdata.DataPoint[int64]{
+											{
+												Value:      42,
+												Attributes: attribute.NewSet(attribute.String("foo", "baz")),
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			}
+			Expect(Index(rms)).Error().To(MatchError(MatchRegexp(
+				`metric "foo.bar" with differing units "megafubars" and "kilofubars"`)))
+		})
+
+		It("rejects differing descriptions", func() {
+			rms := []*metricdata.ResourceMetrics{
+				{
+					ScopeMetrics: []metricdata.ScopeMetrics{
+						{
+							Metrics: []metricdata.Metrics{
+								{
+									Name:        "foo.bar",
+									Description: "The lazy brown fox",
+									Data: metricdata.Gauge[int64]{
+										DataPoints: []metricdata.DataPoint[int64]{
+											{
+												Value:      42,
+												Attributes: attribute.NewSet(attribute.String("foo", "bar")),
+											},
+										},
+									},
+								},
+								{
+									Name:        "foo.bar",
+									Description: "The fury lazy socks",
+									Data: metricdata.Sum[int64]{
+										DataPoints: []metricdata.DataPoint[int64]{
+											{
+												Value:      42,
+												Attributes: attribute.NewSet(attribute.String("foo", "baz")),
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			}
+			Expect(Index(rms)).Error().To(MatchError(MatchRegexp(
+				`metric "foo.bar" with differing descriptions ".+" and ".+"`)))
+		})
+
+		It("rejects differing aggregations", func() {
+			rms := []*metricdata.ResourceMetrics{
+				{
+					ScopeMetrics: []metricdata.ScopeMetrics{
+						{
+							Metrics: []metricdata.Metrics{
+								{
+									Name: "foo.bar",
+									Data: metricdata.Gauge[int64]{
+										DataPoints: []metricdata.DataPoint[int64]{
+											{
+												Value:      42,
+												Attributes: attribute.NewSet(attribute.String("foo", "bar")),
+											},
+										},
+									},
+								},
+								{
+									Name: "foo.bar",
+									Data: metricdata.Sum[int64]{
+										DataPoints: []metricdata.DataPoint[int64]{
+											{
+												Value:      42,
+												Attributes: attribute.NewSet(attribute.String("foo", "baz")),
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			}
+			Expect(Index(rms)).Error().To(MatchError(MatchRegexp(
+				`metric "foo.bar" .* Sum\[int64\] and Gauge\[int64\]`)))
+		})
+
+	})
+
+	It("collects and indexes metrics into families", func() {
 		rms := []*metricdata.ResourceMetrics{
 			{
 				ScopeMetrics: []metricdata.ScopeMetrics{
@@ -32,15 +152,44 @@ var _ = Describe("metrics indices", func() {
 								Name: "foo.bar",
 								Data: metricdata.Gauge[int64]{
 									DataPoints: []metricdata.DataPoint[int64]{
-										{Value: 42},
+										{
+											Value:      42,
+											Attributes: attribute.NewSet(attribute.String("foo", "bar")),
+										},
 									},
 								},
 							},
+						},
+					},
+					{
+						Metrics: []metricdata.Metrics{
 							{
 								Name: "foo.bar",
-								Data: metricdata.Sum[int64]{
+								Data: metricdata.Gauge[int64]{
 									DataPoints: []metricdata.DataPoint[int64]{
-										{Value: 666},
+										{
+											Value:      666,
+											Attributes: attribute.NewSet(attribute.String("foo", "baz")),
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			{
+				ScopeMetrics: []metricdata.ScopeMetrics{
+					{
+						Metrics: []metricdata.Metrics{
+							{
+								Name: "foo.bar",
+								Data: metricdata.Gauge[int64]{
+									DataPoints: []metricdata.DataPoint[int64]{
+										{
+											Value:      123,
+											Attributes: attribute.NewSet(attribute.String("foo", "tada")),
+										},
 									},
 								},
 							},
@@ -49,8 +198,13 @@ var _ = Describe("metrics indices", func() {
 				},
 			},
 		}
-		Expect(Index(rms)).Error().To(MatchError(MatchRegexp(
-			`metric "foo.bar" .* Sum\[int64\] and Gauge\[int64\]`)))
+		Expect(Index(rms)).To(HaveKeyWithValue("foo.bar",
+			ConsistOf(
+				HaveField("Metrics.Data.DataPoints", ConsistOf(
+					HaveField("Value", int64(42)))),
+				HaveField("Metrics.Data.DataPoints", ConsistOf(
+					HaveField("Value", int64(123)))),
+				HaveField("Metrics.Data.DataPoints", ConsistOf(
+					HaveField("Value", int64(666)))))))
 	})
-
 })
